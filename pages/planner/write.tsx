627b1dc0d4f4bff397159cbf134de33dadfeb722
hig4342 <hig4342@naver.com>
2020-02-24 15:41:41 +0900

    fixed theme

--- pages/planner/write.tsx
+++ pages/planner/write.tsx
@@ -156,7 +156,7 @@ const PlannerWrite: NextPage<Props> = ({themes})=> {
           wrapperCol={{span: 24}}
         >
           <Checkbox.Group
-            options={themes.filter(theme => (theme.name !== 'P(편)한 계획표' && theme.name === 'ITP 여행계획표')).map(theme => ({ label: theme.name, value: theme.id }))}
+            options={themes.filter(theme => (theme.name !== 'P(편)한 계획표' && theme.name !== 'ITP 여행계획표')).map(theme => ({ label: theme.name, value: theme.id }))}
           />
         </Form.Item>
         <Form.Item
